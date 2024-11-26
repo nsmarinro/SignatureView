d4cb292ee452cbff9891e77b847e11f6ef46f6af
nsmarinro <nsmarinro@gmail.com>
2024-11-26 13:47:09 -0500

    update Opencv library to 4.10

--- librarySignature/src/main/java/nsmarinro/librarysignature/SignatureView.kt
+++ librarySignature/src/main/java/nsmarinro/librarysignature/SignatureView.kt
@@ -2,9 +2,14 @@ package nsmarinro.librarysignature
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.*
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.support.v4.content.res.ResourcesCompat
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import org.opencv.android.OpenCVLoader
@@ -12,11 +17,9 @@ import org.opencv.core.CvType
 import org.opencv.core.Mat
 import org.opencv.core.MatOfPoint
 import org.opencv.imgproc.Imgproc
-import java.util.*
 import kotlin.math.abs
 
 open class SignatureView : View {
-
     private var mBitmap: Bitmap? = null
     private var mCanvas: Canvas? = null
     private var mPath: Path? = null
@@ -32,10 +35,13 @@ open class SignatureView : View {
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         init()
-
     }
 
-    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle,
+    ) {
         init()
     }
 
@@ -51,10 +57,21 @@ open class SignatureView : View {
         mPaint?.strokeCap = Paint.Cap.ROUND
         mPaint?.strokeWidth = 4f
 
-        this.setBackgroundColor(ResourcesCompat.getColor(context.resources, android.R.color.white, null))
+        this.setBackgroundColor(
+            ResourcesCompat.getColor(
+                context.resources,
+                android.R.color.white,
+                null,
+            ),
+        )
     }
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+    override fun onSizeChanged(
+        w: Int,
+        h: Int,
+        oldw: Int,
+        oldh: Int,
+    ) {
         super.onSizeChanged(w, h, oldw, oldh)
 
         mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
@@ -79,10 +96,12 @@ open class SignatureView : View {
                 startTouch(x, y)
                 invalidate()
             }
+
             MotionEvent.ACTION_MOVE -> {
                 moveTouch(x, y)
                 invalidate()
             }
+
             MotionEvent.ACTION_UP -> {
                 upTouch()
                 invalidate()
@@ -91,18 +110,24 @@ open class SignatureView : View {
         return true
     }
 
-    //private functions
+    // private functions
     private fun upTouch() {
         mPath?.lineTo(mX, mY)
     }
 
-    private fun startTouch(x: Float, y: Float) {
+    private fun startTouch(
+        x: Float,
+        y: Float,
+    ) {
         mPath?.moveTo(x, y)
         mX = x
         mY = y
     }
 
-    private fun moveTouch(x: Float, y: Float) {
+    private fun moveTouch(
+        x: Float,
+        y: Float,
+    ) {
         val dx: Float = abs(x - mX)
         val dy: Float = abs(y - mY)
         if (dx >= TOLERANCE || dy >= TOLERANCE) {
@@ -118,10 +143,12 @@ open class SignatureView : View {
     }
 
     private fun getBitmap(view: View): Bitmap {
-        val bitmap: Bitmap = Bitmap.createBitmap(
-            view.width,
-            view.height, Bitmap.Config.ARGB_8888
-        )
+        val bitmap: Bitmap =
+            Bitmap.createBitmap(
+                view.width,
+                view.height,
+                Bitmap.Config.ARGB_8888,
+            )
         val canvas = Canvas(bitmap)
         bitmap.eraseColor(Color.WHITE)
         canvas.drawRGB(255, 255, 255)
@@ -132,14 +159,16 @@ open class SignatureView : View {
     private fun getImageFromBitmap(bitmap: Bitmap): Mat {
         val img = Mat()
         val bmp32: Bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
-        org.opencv.android.Utils.bitmapToMat(bmp32, img)
+        org.opencv.android.Utils
+            .bitmapToMat(bmp32, img)
 
         return img
     }
 
-    private fun getRoiSignature(inputFrame: Mat, roi: org.opencv.core.Rect): Mat {
-        return Mat(inputFrame, roi)
-    }
+    private fun getRoiSignature(
+        inputFrame: Mat,
+        roi: org.opencv.core.Rect,
+    ): Mat = Mat(inputFrame, roi)
 
     private fun getGrayImage(image: Mat): Mat {
         val imgGray = Mat()
@@ -152,21 +181,24 @@ open class SignatureView : View {
     private fun getBinaryImage(imageGray: Mat): Mat {
         val binary = Mat()
         Imgproc.adaptiveThreshold(
-            imageGray, binary, 255.0, Imgproc.ADAPTIVE_THRESH_MEAN_C,
-            Imgproc.THRESH_BINARY_INV, 11, 12.0
+            imageGray,
+            binary,
+            255.0,
+            Imgproc.ADAPTIVE_THRESH_MEAN_C,
+            Imgproc.THRESH_BINARY_INV,
+            11,
+            12.0,
         )
 
         return binary
     }
 
-    //public methods
+    // public methods
     fun signatureClear() {
         clearCanvas()
     }
 
-    open fun isSignature(): Boolean {
-        return mPath?.isEmpty?.not() ?: false
-    }
+    open fun isSignature(): Boolean = mPath?.isEmpty?.not() ?: false
 
     fun setPathColor(color: Int) {
         mPaint?.color = color
@@ -186,16 +218,19 @@ open class SignatureView : View {
         val height = bitmap.height
         val arrayResult = IntArray(height * width)
 
-        //get pixels array of original image
+        // get pixels array of original image
         bitmap.getPixels(arrayResult, 0, width, 0, 0, width, height)
 
-        //change pixel color one by one
+        // change pixel color one by one
         for (i in 0 until height * width) {
-            if (arrayResult[i] == Color.WHITE) arrayResult[i] = Color.TRANSPARENT
-            else arrayResult[i] = mPaint?.color!!
+            if (arrayResult[i] == Color.WHITE) {
+                arrayResult[i] = Color.TRANSPARENT
+            } else {
+                arrayResult[i] = mPaint?.color!!
+            }
         }
 
-        //copy original image
+        // copy original image
         val resultBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         resultBitmap.setPixels(arrayResult, 0, width, 0, 0, width, height)
         return resultBitmap
@@ -203,16 +238,29 @@ open class SignatureView : View {
 
     fun getSignatureBitmap(drawBoundingBox: Boolean = false): Bitmap? {
         if (isSignature()) {
-            OpenCVLoader.initDebug()
+            val isInitOpenCv: Boolean = OpenCVLoader.initLocal()
+            Log.i("OpenCV", "OpenCV initLocal = $isInitOpenCv")
             val bitmap: Bitmap = getBitmap(this)
             val img: Mat = getImageFromBitmap(bitmap)
             val imgGray: Mat = getGrayImage(img)
             val binary: Mat = getBinaryImage(imgGray)
 
             val contours = ArrayList<MatOfPoint>()
-            Imgproc.findContours(binary, contours, Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE)
+            Imgproc.findContours(
+                binary,
+                contours,
+                Mat(),
+                Imgproc.RETR_EXTERNAL,
+                Imgproc.CHAIN_APPROX_NONE,
+            )
             if (drawBoundingBox) {
-                Imgproc.drawContours(img, contours, -1, org.opencv.core.Scalar(255.0, 255.0, 0.0), 3)
+                Imgproc.drawContours(
+                    img,
+                    contours,
+                    -1,
+                    org.opencv.core.Scalar(255.0, 255.0, 0.0),
+                    3,
+                )
             }
 
             val rectangles = ArrayList<org.opencv.core.Rect>()
@@ -224,39 +272,43 @@ open class SignatureView : View {
                         img,
                         org.opencv.core.Point(
                             mRect.x.toDouble(),
-                            mRect.y.toDouble()
+                            mRect.y.toDouble(),
                         ),
                         org.opencv.core.Point(
                             mRect.x.toDouble() + mRect.width.toDouble(),
-                            mRect.y.toDouble() + mRect.height.toDouble()
+                            mRect.y.toDouble() + mRect.height.toDouble(),
                         ),
                         org.opencv.core.Scalar(0.0, 255.0, 0.0),
-                        2
+                        2,
                     )
                 }
             }
 
             val pointsContour = ArrayList<org.opencv.core.Point>()
             rectangles.forEach { rect: org.opencv.core.Rect ->
-                val point1 = org.opencv.core.Point(
-                    rect.x.toDouble(),
-                    rect.y.toDouble()
-                )
+                val point1 =
+                    org.opencv.core.Point(
+                        rect.x.toDouble(),
+                        rect.y.toDouble(),
+                    )
 
-                val point2 = org.opencv.core.Point(
-                    rect.x.toDouble() + rect.width.toDouble(),
-                    rect.y.toDouble()
-                )
+                val point2 =
+                    org.opencv.core.Point(
+                        rect.x.toDouble() + rect.width.toDouble(),
+                        rect.y.toDouble(),
+                    )
 
-                val point3 = org.opencv.core.Point(
-                    rect.x.toDouble(),
-                    rect.y.toDouble() + rect.height.toDouble()
-                )
+                val point3 =
+                    org.opencv.core.Point(
+                        rect.x.toDouble(),
+                        rect.y.toDouble() + rect.height.toDouble(),
+                    )
 
-                val point4 = org.opencv.core.Point(
-                    rect.x.toDouble() + rect.width.toDouble(),
-                    rect.y.toDouble() + rect.height.toDouble()
-                )
+                val point4 =
+                    org.opencv.core.Point(
+                        rect.x.toDouble() + rect.width.toDouble(),
+                        rect.y.toDouble() + rect.height.toDouble(),
+                    )
 
                 pointsContour.add(point1)
                 pointsContour.add(point2)
@@ -272,56 +324,62 @@ open class SignatureView : View {
                 y.add(point.y)
             }
 
-            val mainRect = org.opencv.core.Rect(
-                x.min().toInt(),
-                y.min().toInt(),
-                x.max().toInt() - x.min().toInt(),
-                y.max().toInt() - y.min().toInt()
-            )
+            val mainRect =
+                org.opencv.core.Rect(
+                    x.min().toInt(),
+                    y.min().toInt(),
+                    x.max().toInt() - x.min().toInt(),
+                    y.max().toInt() - y.min().toInt(),
+                )
 
             if (drawBoundingBox) {
                 Imgproc.rectangle(
                     img,
                     org.opencv.core.Point(
                         mainRect.x.toDouble(),
-                        mainRect.y.toDouble()
+                        mainRect.y.toDouble(),
                     ),
                     org.opencv.core.Point(
                         mainRect.x.toDouble() + mainRect.width.toDouble(),
-                        mainRect.y.toDouble() + mainRect.height.toDouble()
+                        mainRect.y.toDouble() + mainRect.height.toDouble(),
                     ),
                     org.opencv.core.Scalar(0.0, 255.0, 0.0),
-                    2
+                    2,
                 )
             }
 
-            val roiRect = org.opencv.core.Rect(
-                org.opencv.core.Point(
-                    mainRect.x.toDouble(),
-                    mainRect.y.toDouble()
-                ),
-                org.opencv.core.Point(
-                    mainRect.x.toDouble() + mainRect.width.toDouble(),
-                    mainRect.y.toDouble() + mainRect.height.toDouble()
+            val roiRect =
+                org.opencv.core.Rect(
+                    org.opencv.core.Point(
+                        mainRect.x.toDouble(),
+                        mainRect.y.toDouble(),
+                    ),
+                    org.opencv.core.Point(
+                        mainRect.x.toDouble() + mainRect.width.toDouble(),
+                        mainRect.y.toDouble() + mainRect.height.toDouble(),
+                    ),
                 )
-            )
 
             val signatureImage = getRoiSignature(img, roiRect)
-            if (mWidth != null && signatureImage.width().toDouble() > mWidth!!) Imgproc.resize(
-                signatureImage,
-                signatureImage,
-                calculateSize(signatureImage)
-            )
+            if (mWidth != null && signatureImage.width().toDouble() > mWidth!!) {
+                Imgproc.resize(
+                    signatureImage,
+                    signatureImage,
+                    calculateSize(signatureImage),
+                )
+            }
 
-            val bitOutput: Bitmap = Bitmap.createBitmap(
-                signatureImage.cols(),
-                signatureImage.rows()
-                , Bitmap.Config.ARGB_8888
-            )
+            val bitOutput: Bitmap =
+                Bitmap.createBitmap(
+                    signatureImage.cols(),
+                    signatureImage.rows(),
+                    Bitmap.Config.ARGB_8888,
+                )
             bitOutput.eraseColor(Color.WHITE)
             bitOutput.setHasAlpha(false)
 
-            org.opencv.android.Utils.matToBitmap(signatureImage, bitOutput)
+            org.opencv.android.Utils
+                .matToBitmap(signatureImage, bitOutput)
             if (drawBoundingBox) return bitOutput
             return setBackgroundToTransparent(bitOutput)
         }
@@ -331,5 +389,4 @@ open class SignatureView : View {
     companion object {
         private const val TOLERANCE = 5f
     }
-
 }
